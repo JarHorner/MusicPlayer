a1a52247d914714a3dc1113a4460bba05561310a
Jarret Horner <JHorn384@mtroyal.ca>
2023-05-10 17:40:15 -0600

    added popup. working on getting popup working

--- MythicPlusLootTable.lua
+++ MythicPlusLootTable.lua
@@ -1,15 +1,30 @@
 
-SLASH_CHECKSCORE1 = "/cs"
+local keyLevels = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
 
 
-local function checkScoreHandler()
+SLASH_CHECKLOOT1 = "/cl"
 
-    local weeklyRewardLevel, endOfRunRewardLevel = C_MythicPlus.GetRewardLevelForDifficultyLevel(20)
-    print("weekly reward for a 20 key is " .. weeklyRewardLevel)
-    print("End of run reward for a 20 key is " .. endOfRunRewardLevel)
+
+local function checkLootHandler()
+
+    local isMythicPlusActive = C_MythicPlus.IsMythicPlusActive()
+    if (isMythicPlusActive) then
+        print("In M+")
+    else
+        print("Not M+")
+    end
+    local challengeModeActive = C_ChallengeMode.IsChallengeModeActive()
+    if (challengeModeActive) then
+        print("In challenge")
+    else
+        print("Not challenge")
+    end
+
+
+    -- for i = 2, 20, 1 do
+    --     local weeklyRewardLevel, endOfRunRewardLevel = C_MythicPlus.GetRewardLevelForDifficultyLevel(i)
+    --     print("Key level: " .. i .. " weekly reward = " .. weeklyRewardLevel .. " End of run reward = " .. endOfRunRewardLevel)
+    -- end
 end
 
-SlashCmdList["SOUND"] = playSoundHandler
-SlashCmdList["STOPSOUND"] = stopSoundHandler
-SlashCmdList["CHECKSCORE"] = checkScoreHandler
-
+SlashCmdList["CHECKLOOT"] = checkLootHandler
---
+++ MythicPlusPopup.lua
@@ -0,0 +1,18 @@
+
+local isMythicPlusActive = C_MythicPlus.IsMythicPlusActive()
+
+if (isMythicPlusActive) then
+
+    local activeKeystoneLevel = C_ChallengeMode.GetActiveKeystoneInfo()
+    local weeklyRewardLevel, endOfRunRewardLevel = C_MythicPlus.GetRewardLevelForDifficultyLevel(activeKeystoneLevel)
+
+    StaticPopupDialogs["KEY_STARTED"] = {
+        text = "Completion will give " .. endOfRunRewardLevel .. " ilvl end of run, " .. weeklyRewardLevel .. " weekly" ,
+        button1 = "Ok",
+        timeout = 10,
+        whileDead = true,
+        hideOnEscape = true,
+    }
+    
+    StaticPopup_Show("KEY_STARTED", activeKeystoneLevel)
+end
