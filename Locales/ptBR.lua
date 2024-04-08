46da244c7bc68d39e4c54f5d46b5cbd9e572fbe7
Jarret Horner <JHorn384@mtroyal.ca>
2024-04-08 10:22:48 -0600

    Added full Italian and Portuguese translations

--- Locales/ptBR.lua
+++ Locales/ptBR.lua
@@ -1,5 +1,38 @@
 if not ((GAME_LOCALE or GetLocale()) == "ptBR") then
     return
   end
-MPLT = MPLT or {}
-MPLT["Startup Message"] = "Para abrir o Mythic+ Loot Table, usar /showlt. Para abrir as configurações, use /ltsettings"
+  MPLT = MPLT or {}
+  --[[Translation missing --]]
+  MPLT["Addon Name"] = "Mythic Plus Loot Table"
+  MPLT["Disable Keystone Tooltip Toggle"] = "Desativar dica de ferramenta Keystone (requer recarga)"
+  MPLT["Disable Popup Toggle"] = "Desativar Popup"
+  MPLT["End of Dungeon Header"] = "Fim da masmorra"
+  MPLT["Extra Large Size"] = "Extra Grande"
+  MPLT["Extra Small Size"] = "Extra Pequeno"
+  MPLT["Hide Minimap Toggle"] = "Ocultar ícone do minimapa"
+  MPLT["Keystone Hover Text 1"] = "Conclusão:"
+  MPLT["Keystone Hover Text 2"] = "Semanalmente:"
+  MPLT["Large Size"] = "Grande"
+  MPLT["Level Header"] = "Nível"
+  MPLT["Medium Size"] = "Médio"
+  MPLT["No Button"] = "Não"
+  --[[Translation missing --]]
+  MPLT["Popup Button"] = "Ok"
+  MPLT["Popup Text 1"] = "A conclusão dará"
+  MPLT["Popup Text 2"] = "fim da corrida,"
+  MPLT["Popup Text 3"] = "semanalmente"
+  MPLT["Popup Timer Text"] = "Tempo de pop-up do saque M+"
+  MPLT["Reset Settings Text"] = "Tem certeza de que deseja redefinir as variáveis ​​salvas? usar /reload para garantir que as alterações sejam feitas"
+  MPLT["Reset Variables Button"] = "Redefinir variáveis ​​de complemento"
+  MPLT["Size of Table"] = "Tamanho da mesa"
+  MPLT["Small Size"] = "pequeno"
+  MPLT["Startup Message"] = "Para abrir o Mythic+ Loot Table, usar /showlt. Para abrir as configurações, use /ltsettings"
+  MPLT["Table Note"] = "O nível de atualização do equipamento está entre colchetes"
+  MPLT["Tooltip Text 1"] = "Clique esquerdo: Alternar janela da tabela"
+  MPLT["Tooltip Text 2"] = "Clique com o botão direito: abrir menu de opções"
+  MPLT["Upgrade Track Champion"] = "Campeão"
+  MPLT["Upgrade Track Hero"] = "Herói"
+  MPLT["Upgrade Track Myth"] = "Mito"
+  MPLT["Upgrade Track Veteran"] = "Veterano"
+  MPLT["Vault Header"] = "Cofre"
+  MPLT["Yes Button"] = "Sim"
