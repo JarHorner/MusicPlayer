0e1ec337d7c3f509ff42e71f8c4364035348d916
Jarret Horner <JHorn384@mtroyal.ca>
2024-04-25 02:00:23 -0600

    Changes to text and code for popup

--- MythicPlusPopup.lua
+++ MythicPlusPopup.lua
@@ -9,20 +9,30 @@ local function ShowKeyLevelLoot()
         if activeKeystoneLevel <= 10 then
             local endOfRunRewardLevel = lootTableData[activeKeystoneLevel][1] .. " | " .. lootTableData[activeKeystoneLevel][2]
             local weeklyRewardLevel = lootTableData[activeKeystoneLevel][3] .. " | " .. lootTableData[activeKeystoneLevel][4]
+
+            StaticPopupDialogs["KEY_STARTED"] = {
+                text = MPLT["Popup Text 1"] .. endOfRunRewardLevel .. "\n\n" .. MPLT["Popup Text 2"] .. weeklyRewardLevel ,
+                button1 = MPLT["Popup Button"],
+                timeout = SavedVariables.popupTime,
+                whileDead = true,
+                hideOnEscape = true,
+            }
+            
+            StaticPopup_Show("KEY_STARTED", activeKeystoneLevel)
         else
             local endOfRunRewardLevel = lootTableData[10][1] .. " | " .. lootTableData[10][2]
             local weeklyRewardLevel = lootTableData[10][3] .. " | " .. lootTableData[10][4]
-        end
+
+            StaticPopupDialogs["KEY_STARTED"] = {
+                text = MPLT["Popup Text 1"] .. endOfRunRewardLevel .. "\n\n" .. MPLT["Popup Text 2"] .. weeklyRewardLevel ,
+                button1 = MPLT["Popup Button"],
+                timeout = SavedVariables.popupTime,
+                whileDead = true,
+                hideOnEscape = true,
+            }
             
-        StaticPopupDialogs["KEY_STARTED"] = {
-            text = MPLT["Popup Text 1"] .. endOfRunRewardLevel .. MPLT["Popup Text 2"] .. weeklyRewardLevel .. MPLT["Popup Text 3"] ,
-            button1 = MPLT["Popup Button"],
-            timeout = SavedVariables.popupTime,
-            whileDead = true,
-            hideOnEscape = true,
-        }
-        
-        StaticPopup_Show("KEY_STARTED", activeKeystoneLevel)
+            StaticPopup_Show("KEY_STARTED", activeKeystoneLevel)
+        end
     end
 end
 
